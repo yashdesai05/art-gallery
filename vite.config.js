aa6e282ffa080fd53f1051bf2b2dfe76f2385315
Yash Desai <yash.desai@merai.co>
2026-06-11 09:50:00 +0530

    fix: remove unused react plugin to fix build error

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh' // ya taro jo pan plugin hoy
 
 export default defineConfig({
-  plugins: [react()],
   server: {
     allowedHosts: [
-      'art-gallery-kbe1.onrender.com' // Aya taro render valo host nakhvo
+      'art-gallery-kbe1.onrender.com'
     ]
   }
 })
